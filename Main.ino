76f5da5794da3f6016c85a76a595b05f50fc16bb
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-11-14 09:46:58 +0800

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,50 +1,32 @@
-const int pinSensor = A2;
-const int pinled = 6;
-const int sensorpir = 4;
-int data ;
-int statuspir = 0;
-
-//brightness = 0;
+//Var_Declare
+uint8_t LDR = A2,uint8_t pinled = 6,uint8_t sensorpir = 4;
+uint32_t data;
+bool PIR = 0;
 
 void setup() {
-  pinMode(pinSensor, INPUT);
+  
+  pinMode(pinled, INPUT);
   pinMode(sensorpir, INPUT);
   pinMode(pinled, OUTPUT);
+  digitalWrite(pinled,LOW);
   Serial.begin(9600);
 
 }
 
-void loop() {
-  data = analogRead(pinSensor);
-  statuspir = digitalRead (sensorpir);
+void loop(){
+
+  //SENSOR_READING---------------------
+  PIR= (bool) digitalRead(sensorpir);
+  data=analogRead(LDR);
+  //-----------------------------------
+
+  Serial.println(PIR);  
   Serial.println(data);
+  //LOGIC_SECTION-----------
+  if(PIR==1 && data<=500){
+    digitalWrite(pinled,HIGH);    
+  }else digitalWrite(pinled,LOW);
 
-  if (data>= 500){
-    analogWrite(pinled, LOW);
-//    if(statuspir == HIGH){
-//      digitalWrite(pinled, HIGH);
-//      Serial.println("ada gerakan");
-//    }
-//    else{
-//      digitalWrite(pinled, LOW);
-//      Serial.println("tidak ada gerakan");
-//    }
-    
-  }
-
-  else if (data<= 500){
-    if(statuspir == HIGH){
-      analogWrite(pinled, HIGH);
-      Serial.println("ada gerakan");
-    }
-    else{
-      analogWrite(pinled, LOW);
-      Serial.println("tidak ada gerakan");
-    }
-    
-//      digitalWrite(pinled, HIGH);
-
-  }
-  delay(1000);
+ 
 }
 
